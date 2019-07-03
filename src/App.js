050528c69d718b7d5559266480c841c635da62fe
Danilov Igor <danilovigor773@gmail.com>
2019-07-03 19:04:17 +0500

    Created starter project with react

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
+
+class App extends React.Component {
+  handleTitleClick() {
+    alert("you clicked the title!");
+  }
+  render() {
+    return (
+      <div>
+        <h1 onClick={this.handleTitleClick}>Adopt Me!</h1>
+        <Pet name="Luna" animal="dog" breed="Havanese" />
+        <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+        <Pet name="Doink" animal="cat" breed="Mixed" />
+      </div>
+    );
+  }
+}
+
+render(React.createElement(App), document.getElementById("root"));
